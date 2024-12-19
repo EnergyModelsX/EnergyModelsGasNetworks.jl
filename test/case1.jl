b55c66df37e5b73b137ea4be0c681aa4771e2b6d
Raquel Alonso <raquel.alonso@sintef.no>
2024-12-19 10:56:41 +0100

    Adjusted test for new Component and removal of BlendAvailability and RefBlending nodes

--- test/case1.jl
+++ test/case1.jl
@@ -2,11 +2,13 @@
 function generate_case()
 
     # Define resources
-    NG = EMP.ResourceComponent("NG")
-    H2 = EMP.ResourceComponentTrack("H2")
-    Blend = EMP.ResourceBlend("Blend", [NG, H2])
+    NG = EMP.RefComponent("NG", 0.0)
+    H2 = EMP.ComponentTrack("H2", 0.0)
+    
+    Gas = EMB.ResourceCarrierBlend("Gas", [NG, H2])
     CO2 = EMB.ResourceEmit("CO2", 1.0)
-    products = [NG, H2, Blend, CO2]
+    products = [CO2, Gas]
+    components = [NG, H2]
 
     # Time
     op_duration = 1
@@ -34,30 +36,12 @@ function generate_case()
             FixedProfile(150), # Capacity
             FixedProfile(0), # Var. OPEX
             FixedProfile(0), # Fix. OPEX
-            Dict(NG => 1), # Output
-            Dict(NG => 1)
-        ),
-        RefSourceComponent(
-            103,
-            FixedProfile(150), # Capacity
-            FixedProfile(0), # Var. OPEX
-            FixedProfile(0), # Fix. OPEX
-            Dict(H2 => 1), # Output
+            Dict(Gas => 1), # Output
             Dict(H2 => 1)
         ),
-        RefBlending(
-            104,
-            FixedProfile(150), # Capacity
-            FixedProfile(0), # Var. OPEX
-            FixedProfile(0), # Fix. OPEX
-            Dict(H2 => 1, NG => 1), # Input
-            Dict(Blend => 1) # Output
-        )
     ]
     l = [
-        Direct(110, n[2], n[4], Linear()),
-        Direct(120, n[3], n[4], Linear()),
-        Direct(130, n[4], n[1], Linear())
+        Direct(130, n[2], n[1], Linear())
     ]
     append!(nodes, n)
     append!(links, l)
@@ -71,7 +55,7 @@ function generate_case()
             FixedProfile(100), # Capacity
             FixedProfile(0), # Var. OPEX
             FixedProfile(0), # Fix. OPEX
-            Dict(NG => 1), # Output
+            Dict(Gas => 1), # Output
             Dict(NG => 1)
         )
     ]
@@ -90,7 +74,7 @@ function generate_case()
             FixedProfile(50), # Capacity
             FixedProfile(0), # Var. OPEX
             FixedProfile(0), # Fix. OPEX
-            Dict(NG => 1), # Output
+            Dict(Gas => 1), # Output
             Dict(NG => 1)
         )
     ]
@@ -103,19 +87,9 @@ function generate_case()
 
     # Nodes in Area 4
     n = [
-        GeoAvailability(401, products),
-        RefBlending(
-            402,
-            FixedProfile(300), # Capacity
-            FixedProfile(0), # Var. OPEX
-            FixedProfile(0), # Fix. OPEX
-            Dict(Blend => 1, NG => 1), # Input
-            Dict(Blend => 1) # Output
-        )
+        GeoAvailability(401, products)
     ]
     l = [
-        Direct(410, n[2], n[1], Linear())
-        Direct(420, n[1], n[2], Linear())
     ]
     append!(nodes, n)
     append!(links, l)
@@ -123,19 +97,9 @@ function generate_case()
 
     # Nodes in Area 5
     n = [
-        GeoAvailability(501, products),
-        RefBlending(
-            502,
-            FixedProfile(300), # Capacity
-            FixedProfile(0), # Var. OPEX
-            FixedProfile(0), # Fix. OPEX
-            Dict(H2 => 1, NG => 1), # Input
-            Dict(Blend => 1) # Output
-        )
+        GeoAvailability(401, products)
     ]
     l = [
-        Direct(510, n[2], n[1], Linear())
-        Direct(520, n[1], n[2], Linear())
     ]
     append!(nodes, n)
     append!(links, l)
@@ -148,9 +112,9 @@ function generate_case()
             602,
             FixedProfile(100), # Capacity
             Dict(:price => FixedProfile(-190)), # Penalty
-            Dict(H2 => 1, NG => 1, Blend => 1), # Input
-            Dict(H2 => 1, NG => 1, Blend => 1), # upperbound
-            Dict(H2 => 0.2) # lowerbound
+            Dict(Gas => 1), # Input
+            Dict(H2 => 0.2, NG => 1), # upperbound
+            Dict(H2 => 0) # lowerbound
         )
     ]
     l = [
@@ -167,9 +131,9 @@ function generate_case()
             702,
             FixedProfile(50), # Capacity
             Dict(:price => FixedProfile(-190)), # Penalty
-            Dict(H2 => 1, NG => 1, Blend => 1), # Input
-            Dict(H2 => 1, NG => 1, Blend => 1), # upperbound
-            Dict(H2 => 0.2) # lowerbound
+            Dict(Gas => 1), # Input
+            Dict(H2 => 1, NG => 1), # upperbound
+            Dict(H2 => 0) # lowerbound
         )
     ]
     l = [
@@ -194,16 +158,16 @@ function generate_case()
     # Note: The inlet and outlets do not affect as the trans_out are not associated to Resources
     # and the different flows are tracked by prop_source variable
     fixed_O = FixedProfile(0.0)
-    tm_14 = PipeSimple("tm_14", Blend, Blend, Blend, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_15 = PipeSimple("tm_15", Blend, Blend, Blend, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_24 = PipeSimple("tm_24", NG, NG, NG, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_25 = PipeSimple("tm_25", NG, NG, NG, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_34 = PipeSimple("tm_34", NG, NG, NG, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_35 = PipeSimple("tm_35", NG, NG, NG, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_46 = PipeSimple("tm_46", NG, NG, NG, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_47 = PipeSimple("tm_47", NG, NG, NG, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_56 = PipeSimple("tm_56", NG, NG, NG, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
-    tm_57 = PipeSimple("tm_57", NG, NG, NG, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_14 = PipeSimple("tm_14", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_15 = PipeSimple("tm_15", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_24 = PipeSimple("tm_24", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_25 = PipeSimple("tm_25", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_34 = PipeSimple("tm_34", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_35 = PipeSimple("tm_35", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_46 = PipeSimple("tm_46", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_47 = PipeSimple("tm_47", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_56 = PipeSimple("tm_56", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
+    tm_57 = PipeSimple("tm_57", Gas, Gas, Gas, fixed_O, FixedProfile(1e6), fixed_O, fixed_O, fixed_O, 1, Data[])
 
     # Create transmission corriders between areas
     transmission = [
@@ -225,6 +189,7 @@ function generate_case()
         :nodes          => Array{EMB.Node}(nodes),
         :links          => Array{Link}(links),
         :products       => products,
+        :components     => components,
         :T              => T,
         :pwa            => nothing
     )    
