5068a9c305abfc2c214dbd299e48a5421e445b3d
Raquel Alonso <raquel.alonso@sintef.no>
2025-01-21 13:09:21 +0100

    remove previous generation of pwa

--- src/utils.jl
+++ src/utils.jl
@@ -53,38 +53,3 @@ function getsource(a::Area, links)
     source_nodes   = [i for i in EMG.getnodesinarea(a, links) if EMB.is_source(i)]
     return source_nodes
 end
-
-function pwa(optimizer, K)
-    Mᶜᴴ⁴ = 16.042 # molecular weight
-    Mᴴ² = 2.016
-
-    f(X) = sqrt(K) .* sqrt.(X[:, 1].^2 - X[:, 2].^2) ./ sqrt.(Mᶜᴴ⁴ .* (1 .- X[:, 3]) .+ Mᴴ² .* X[:, 3])
-
-    p1 = [i for i ∈ 30:5:70]
-    p2 = [j for j ∈ 30:5:70]
-    prop_H2 = [j for j ∈ 0:0.1:0.2]
-
-    X = hcat(
-        repeat(p1, inner = [length(p2) * length(prop_H2)]),
-        repeat(p2, inner = [length(prop_H2)], outer = [length(p1)]),
-        repeat(prop_H2, outer = [length(p1) * length(p2)])
-    )
-    valid_indices = X[:, 1].^2 .>= X[:, 2].^2
-    X = X[valid_indices, :]
-
-    z = f(X) 
-
-    pwa = approx(
-    FunctionEvaluations(tuple.(eachcol(X)...), z),
-    Concave(),
-    # MILP(
-    Cluster(
-        ;optimizer,
-        planes = 10,
-        strict = :none,
-        metric = :l1,
-    )
-    )
-
-    return pwa
-end
