47cdeffa5aa22c0b6ff35093cdfa4bd94ba65096
Raquel Alonso <raquel.alonso@sintef.no>
2025-03-18 13:22:32 +0100

    Adjust PressBlendPipe struct to include new weymouth_constant calculation

--- src/structures/data.jl
+++ src/structures/data.jl
@@ -10,28 +10,32 @@ end
 struct PressBlendPipe <: PressureData
     id::Any
     max_pressure::Int
-    weymouth::Float64
+    weymouth::Float64   # Weymouth constant normalised with respect to specific gravity
     pwa::Any
 end
 function PressBlendPipe(
         id, max_pressure, optimizer; 
-        weymouth=58, 
+        FLOW::Any, # CH4 flow in accordance with Weymouth equation for a given pressure drop
+        PIN::Any, # Inlet pressure corresponding to FLOW
+        POUT::Any, # Outlet pressure corresponding to FLOW
         pin = [50, 53, 58, 58, 60, 63, 65, 67, 70], 
         pout = [30, 34, 35, 37, 43, 43, 45, 40, 50],
         c2_fraction = [0.0, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.05, 0.1],
         M1 = 16.042,
 		M2 = 2.016
         )
+    
+    weymouth_ct = weymouth_constant(FLOW, PIN, POUT)
 
-    z = weymouth_specgrav.(weymouth, pin, pout, c2_fraction, M1, M2)
+    flow = weymouth_specgrav.(weymouth_ct, pin, pout, c2_fraction, M1, M2)
 
-    fn = get_input_fn([weymouth, pin, pout, c2_fraction], z)
+    fn = get_input_fn([weymouth_ct, pin, pout, c2_fraction], flow)
 
     if isfile(fn)
         pwa = read_from_json(fn)
     else
         pwa = approx(   
-            FunctionEvaluations(collect(zip(pin, pout, c2_fraction)), z),
+            FunctionEvaluations(collect(zip(pin, pout, c2_fraction)), flow),
             Concave(),
             Cluster(
                 ;optimizer,
@@ -45,7 +49,7 @@ function PressBlendPipe(
     return PressBlendPipe(
         id,
         max_pressure,
-        weymouth,
+        weymouth_ct,
         pwa
     )
 end
