090a5b865850db85f63d4b5aa56350b595f1808e
Raquel Alonso <raquel.alonso@sintef.no>
2025-10-10 14:58:34 +0200

    Add a check of the Taylor approximation

--- src/utils.jl
+++ src/utils.jl
@@ -110,3 +110,12 @@ function test_approx(pwa, constant, pin, pout, prop, molmass_other, molmass_trac
                 PiecewiseAffineApprox.evaluate(pwa, (pin, p_out, prop))>=calculate_flow(constant, pin, p_out, prop, molmass_other, molmass_track))
     end
 end
+function test_approx(weymouth, pressure_points::Vector, link_potential_in, link_potential_out)
+    rhs = Float64[]
+    for (p_in, p_out) ∈ pressure_points
+        value = sqrt(weymouth) * 
+        ((p_in/(sqrt(p_in^2 - p_out^2))) * link_potential_in - (p_out/(sqrt(p_in^2 - p_out^2))) * link_potential_out)
+        push!(rhs, value)
+    end 
+    return minimum(rhs)
+end
