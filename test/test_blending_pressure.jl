6d00ab331164b004757b9bc17d0fb6da7df427ae
Lars Hellemo <Lars.Hellemo@sintef.no>
2025-10-16 12:36:23 +0200

    Fix typos

--- test/test_blending_pressure.jl
+++ test/test_blending_pressure.jl
@@ -140,12 +140,12 @@ Blend = first(filter(p -> p.id == "Blend", 𝒫))
 @testset "Basic case - results" begin
     @test JuMP.termination_status(m) in [MOI.OPTIMAL, MOI.OTHER_LIMIT]
 
-    @test m[:link_in][ℒ[1], first(collect(𝒯)), H2] ≈ 0
+    @test value(m[:link_in][ℒ[1], first(collect(𝒯)), H2]) ≈ 0
     @test value(m[:link_in][ℒ[2], first(collect(𝒯)), CH4]) ≈ 20.108  rtol = 0.05
     @test value(m[:link_in][ℒ[3], first(collect(𝒯)), CH4]) ≈ 42.71 atol = 1e-1
     @test value(m[:link_in][ℒ[4], first(collect(𝒯)), Blend]) ≈ 62.82 atol = 1e-1
-    @test value(m[:proportion_track][𝒩[5], first(collect(𝒯)), H2]) ≈ 0.00
-    @test value(m[:proportion_track][𝒩[5], first(collect(𝒯)), CH4]) ≈ 1.00
+    @test value(m[:proportion_track][𝒩[5], first(collect(𝒯)), H2]) ≈ 0.0
+    @test value(m[:proportion_track][𝒩[5], first(collect(𝒯)), CH4]) ≈ 1.0
 end
 
 @testset "Basic case - approximation" begin
@@ -192,7 +192,7 @@ Blend = first(filter(p -> p.id == "Blend", 𝒫))
 
     @test value(m[:link_in][ℒ[1], first(collect(𝒯)), H2]) ≈ 6.50 atol = 1e-1
     @test value(m[:link_in][ℒ[2], first(collect(𝒯)), CH4]) ≈ 15.841 atol = 1e-1
-    @test value(m[:link_in][ℒ[3], first(collect(𝒯)), CH4]) ≈ 42.67; atol = 1e-1
+    @test value(m[:link_in][ℒ[3], first(collect(𝒯)), CH4]) ≈ 42.67 atol = 1e-1
     @test value(m[:link_in][ℒ[4], first(collect(𝒯)), Blend]) ≈ 65.0  rtol = 0.05
     @test value(m[:proportion_track][𝒩[5], first(collect(𝒯)), H2]) ≈ 0.1
     @test value(m[:proportion_track][𝒩[5], first(collect(𝒯)), CH4]) ≈ 0.90
